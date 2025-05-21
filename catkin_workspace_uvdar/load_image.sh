c7ba18a777527dfce4fb9a0bd7a3bf799f52a4f0
manue <manuel.boldrer@gmail.com>
2025-05-21 09:26:45 +0200

    removing ansible installation from script

--- catkin_workspace_uvdar/load_image.sh
+++ catkin_workspace_uvdar/load_image.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-# Check if Ansible is installed
-if ! command -v ansible &> /dev/null; then
-    echo "Ansible is not installed."
-
-    # Ask for user confirmation
-    read -p "Do you want to install Ansible? (y/n): " choice
-    case "$choice" in 
-      y|Y ) 
-        echo "Installing Ansible..."
-        sudo apt update -y && sudo apt install -y ansible
-        echo "Ansible has been installed."
-        ;;
-      n|N ) 
-        echo "Installation canceled."
-        exit 1
-        ;;
-      * ) 
-        echo "Invalid input. Please enter y or n."
-        exit 1
-        ;;
-    esac
-else
-    echo "OK. Ansible is already installed."
-fi
 ansible-playbook -i inventory.ini load_docker_image.yaml --ask-become-pass
